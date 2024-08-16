61fc9f47a47e77be193fc55e66f956b5ee63e5b1
Mateo Fuertes <132864872+mateofuertes@users.noreply.github.com>
2024-08-16 12:08:30 +0200

    Comment date_service.dart

--- src/Flutter App/services/date_service.dart
+++ src/Flutter App/services/date_service.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
+/// [DateService] handles interaction with the backend server for synchronizing the date of
+/// the Raspberry Pi with the date of the phone.
 class DateService {
-    
+    /// Sends the current date and time to the server to modify the date on the Raspberry Pi.
     static Future<String> sendDate(String baseUrl) async {
-    final String date = DateTime.now().toLocal().toString().split('.')[0];
-    debugPrint(date);
-    final response = await http.post(Uri.parse('$baseUrl/set_date'), body: {'new_date': date});
-
+      /// Gets the current date and time.
+      final String date = DateTime.now().toLocal().toString().split('.')[0];
+      final response = await http.post(Uri.parse('$baseUrl/set_date'), body: {'new_date': date});
     if (response.statusCode == 200) {
       String result = json.decode(response.body);      
       return result;
