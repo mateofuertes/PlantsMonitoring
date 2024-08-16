1f108760b24bc7566b2f1d1732319a7b0a03740a
Mateo Fuertes <132864872+mateofuertes@users.noreply.github.com>
2024-08-16 11:36:45 +0200

    Comment date_utils.dart

--- src/Flutter App/utils/date_utils.dart
+++ src/Flutter App/utils/date_utils.dart
@@ -1,4 +1,11 @@
+/// [MyDateUtils] is a utility class that provides static methods for handling
+/// and formatting dates.
 class MyDateUtils {
+  
+  /// Formats a given [DateTime] object into a readable string.
+  ///
+  /// If the given [date] corresponds to today's date, it returns 'Today'.
+  /// Otherwise, it returns the date in 'day/month/year' format.
   static String formatDate(DateTime date) {
     if (date.day == DateTime.now().day &&
         date.month == DateTime.now().month &&
@@ -8,6 +15,11 @@ class MyDateUtils {
     return '${date.day}/${date.month}/${date.year}';
   }
 
+  /// Extracts a date from a given [fileName] string using a regular expression.
+  ///
+  /// The method searches the [fileName] for a date in the format 'yyyy-mm-dd'.
+  /// If a date is found, it returns the matched date string.
+  /// If no date is found, it returns 'Unknown date'.
   static String extractDate(String fileName) {
     RegExp regex = RegExp(r'(\d{4}-\d{2}-\d{2})');
     Match? match = regex.firstMatch(fileName);
