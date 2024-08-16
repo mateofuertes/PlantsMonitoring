f1d4f1fb547ed956ba4a0b631b16144f2ba9fc61
Mateo Fuertes <132864872+mateofuertes@users.noreply.github.com>
2024-08-16 12:44:21 +0200

    Comment progress_screen.dart

--- src/Flutter App/screens/progress_screen.dart
+++ src/Flutter App/screens/progress_screen.dart
@@ -4,6 +4,8 @@ import 'package:app/providers/app_provider.dart';
 import 'package:app/widgets/progress_widget.dart';
 import 'package:app/widgets/download_dialog.dart';
 
+/// [ProgressScreen] is a stateful widget that displays a screen showing daily images and
+/// provides a button to download the images.
 class ProgressScreen extends StatefulWidget {
   const ProgressScreen({super.key});
 
@@ -12,11 +14,13 @@ class ProgressScreen extends StatefulWidget {
   _ProgressScreenState createState() => _ProgressScreenState();
 }
 
+ // The state for this widget
 class _ProgressScreenState extends State<ProgressScreen> {
   @override
   void initState() {
     super.initState();
     try {
+    // Fetch images from the server after the widget's build is complete.
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final provider = Provider.of<AppProvider>(context, listen: false);
       provider.fetchImages();
@@ -29,9 +33,11 @@ class _ProgressScreenState extends State<ProgressScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Access the AppProvider to get the current state of the app.
     final provider = Provider.of<AppProvider>(context);
 
     return Scaffold(
+      // Main body of the screen, displaying either a loading indicator or progress data.
       body: provider.isLoading
           ? const Center(
               child: CircularProgressIndicator(
@@ -39,6 +45,7 @@ class _ProgressScreenState extends State<ProgressScreen> {
               ),
             )
           : ProgressWidget(imagesByDate: provider.imagesByDate),
+      // Floating action button to open the download options dialog.
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showDownloadOptions(context),
         backgroundColor: Colors.green,
@@ -47,6 +54,7 @@ class _ProgressScreenState extends State<ProgressScreen> {
     );
   }
 
+  /// Shows the download options dialog to allow the user to download images.
   void _showDownloadOptions(BuildContext context) {
     showDialog(
       context: context,
