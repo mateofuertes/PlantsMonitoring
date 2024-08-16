76891cfa23c643ee9a6e67d7f7686d0ef9a37679
Mateo Fuertes <132864872+mateofuertes@users.noreply.github.com>
2024-08-16 11:44:14 +0200

    Comment detections_widget.dart

--- src/Flutter App/widgets/detections_widget.dart
+++ src/Flutter App/widgets/detections_widget.dart
@@ -3,15 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:app/providers/app_provider.dart';
 
+/// [DetectionsWidget] is a stateless widget that displays detection images for a selected date.
+/// Based on the current state of the [AppProvider] (loading, no detections, or detections available),
+/// it displays a loading indicator, a message, or a grid of images.
 class DetectionsWidget extends StatelessWidget {
   const DetectionsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Access the AppProvider to get the current state of the app.
     final provider = Provider.of<AppProvider>(context);
+
+    // Extract the selected date from the provider and format it as a string.
     String selectedDate = MyDateUtils.extractDate(provider.selectedDay.toIso8601String());
+
+    // Get the list of images corresponding to the selected date. If no images are found,
+    // return an empty list.
     List<String> imagesForSelectedDay = provider.imagesByDate[selectedDate] ?? [];
 
+    // Build the UI based on the current state of the provider.
     return Expanded(
       child: provider.isLoading
           ? const Center(
@@ -19,6 +29,7 @@ class DetectionsWidget extends StatelessWidget {
                 valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
               ),
             )
+          // If there are no images for the selected date, display a message indicating no detections.
           : imagesForSelectedDay.isEmpty
               ? const Center(
                   child: Text(
@@ -26,6 +37,7 @@ class DetectionsWidget extends StatelessWidget {
                     style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
                   ),
                 )
+              // If there are images, display them in a grid view.
               : GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 1,
